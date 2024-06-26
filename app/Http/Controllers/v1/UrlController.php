71cd007fe4b2ed1c0d68b5a20e9e5aa48a5103cc
Kirithikka <kirithikka@contento.io>
2024-06-26 23:00:32 +0100

    adding model and controller files

---
+++ app/Http/Controllers/v1/UrlController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\v1;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UrlController extends Controller
+{
+    /**
+     * Encode the original url to short url
+     */
+    public function encode(Request $request) {
+    }
+
+    /**
+     * Decode the short url to original url
+     */
+    public function decode(Request $request) {
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\v1\UrlController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('v1')
+    ->group(static function() {
+        Route::post('/encode', [UrlController::class, 'encode'])->name('encode');
+        Route::post('/decode', [UrlController::class, 'decode'])->name('decode');
+    });
